97db8a5a0f01f35696252f2e92dd4366b12cc26a
ahmedSadoun <ahmedsaadoun866@gmail.com>
2024-01-17 11:44:50 +0200

    Added All Attachment On Desktop

---
+++ test/test.js
@@ -0,0 +1,16 @@
+let obj = {
+  "Line Manager Task": "SEC_1",
+  "Employee1 Task": "SEC_1,SEC_2",
+  "Employee2 Task": "SEC_1,SEC_3",
+  "Employee3 Task": "SEC_1,SEC_4",
+  "Employee4 Task": "SEC_1,SEC_5",
+  "Employee5 Task": "SEC_1,SEC_6",
+  "Employee6 Task": "SEC_1,SEC_7",
+  "Employee7 Task": "SEC_1,SEC_8",
+  "Employee8 Task": "SEC_1,SEC_9",
+  "Employee9 Task": "SEC_1,SEC_10",
+  "Payroll Administrator Task":
+    "SEC_1,SEC_2,SEC_3,SEC_4,SEC_5,SEC_6,SEC_7,SEC_8,SEC_9,SEC_10,SEC_11",
+};
+
+console.log(obj["Employee1 Task"]);
